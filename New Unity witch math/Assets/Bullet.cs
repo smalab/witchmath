bace8b59ae5121055a175491effe1d320c3ab51b
showten <shoeten060522@icloud.com>
2014-09-18 15:36:06 +0900

    すべてのBulletに値を当てれるように

--- New Unity witch math/Assets/Bullet.cs
+++ New Unity witch math/Assets/Bullet.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class Bullet: MonoBehaviour {
 	public GUIText myText;  
-	public int k =0;
+	public int bulletValue =0;
 	
 	// Use this for initialization
 	void Start () {
 		
-		myText.text = ""+k;	
+		myText.text = ""+bulletValue;	
 		
 	}
 	// Use this for initialization
--- New Unity witch math/Assets/BulletMaster.cs
+++ New Unity witch math/Assets/BulletMaster.cs
@@ -8,34 +8,52 @@ public class BulletMaster : MonoBehaviour {
 	public GameObject CenterPlus;
 	public GameObject UnderNum;
 
-	public static int CenterNum=3;
-
+	int[] rndArray = new int[9] ;
+	GameObject[] bulletArray= new GameObject[9];
+	     
 	// Use this for initialization
 	void Start () {
 
+		int rnd;
+		for(int i=1;i<10;i++){
+
+
+			do{
+			   rnd=Random.Range (0, 9);
+
+			}while(rndArray[rnd]!=0);
+
+			rndArray[rnd]=i;
+
+		}
+
+
 		Instantiate(CenterPlus, new Vector3(0.23f, 0.4f, 0), Quaternion.identity);
-
-		if(CenterNum==3)
 		Instantiate(CenterPlus, new Vector3(0.43f, 0.4f, 0), Quaternion.identity);
-
 		Instantiate(CenterPlus, new Vector3(0.63f, 0.4f, 0), Quaternion.identity);
 
+		float bulletPos=0.03f;
 
-		GameObject num1 =Instantiate(UnderNum, new Vector3(0.03f, 0.2f, 0), Quaternion.identity)as GameObject;
-		Bullet b = num1.GetComponent<Bullet>();
-		b.k=1;
+		for(int i=0;i<9;i++){
+
+	        if(i%2==0){
+
+				GameObject num =Instantiate(UnderNum, new Vector3(bulletPos+(i/10f), 0.2f, 0), Quaternion.identity)as GameObject;
+				Bullet b = num.GetComponent<Bullet>();
+				b.bulletValue=rndArray[i];
+				bulletArray[i]=num;
+
+			}else{
+
+				GameObject num =Instantiate(UnderNum, new Vector3(bulletPos+(i/10f), 0.1f, 0), Quaternion.identity)as GameObject;
+				Bullet b = num.GetComponent<Bullet>();
+				b.bulletValue=rndArray[i];
+				bulletArray[i]=num;
+			}
+
+		}
+
 	
-	
-		Instantiate(UnderNum, new Vector3(0.23f, 0.2f, 0), Quaternion.identity);
-		Instantiate(UnderNum, new Vector3(0.43f, 0.2f, 0), Quaternion.identity);
-		Instantiate(UnderNum, new Vector3(0.63f, 0.2f, 0), Quaternion.identity);
-		Instantiate(UnderNum, new Vector3(0.83f, 0.2f, 0), Quaternion.identity);
-
-
-		Instantiate(UnderNum, new Vector3(0.13f, 0.1f, 0), Quaternion.identity);
-		Instantiate(UnderNum, new Vector3(0.33f, 0.1f, 0), Quaternion.identity);
-		Instantiate(UnderNum, new Vector3(0.53f, 0.1f, 0), Quaternion.identity);
-		Instantiate(UnderNum, new Vector3(0.73f, 0.1f, 0), Quaternion.identity);
 
 	}
 	
